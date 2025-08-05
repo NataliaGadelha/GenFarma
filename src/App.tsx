1e807a2ea53d75e03f3568032686e5d9556e7ca0
NataliaGadelha <natt.gdl25@gmail.com>
2025-08-05 15:04:46 -0300

    feat: criação do projeto

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import './App.css'
+import ListaCategorias from './components/categorias/listacategorias/ListaCategorias'
+import { ToastContainer } from 'react-toastify'
+import FormCategorias from './components/categorias/formcategorias/FormCategorias'
+import DeletarCategoria from './components/categorias/deletarcategorias/DeletarCategoria'
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastrarcategorias" element={<FormCategorias />} />
+              <Route path="/editarcategoria/:id" element={<FormCategorias />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      <ToastContainer/>
+    </>
+  )
+}
+
+export default App
